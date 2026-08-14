ac45d40e7b568fa5919296cedaa3d0fd7acf8590
Lucas Sala <proyectocaecus@gmail.com>
2026-08-14 12:57:44 -0300

    Update camera ID and device secret in device_config.h

--- main/device_config.h
+++ main/device_config.h
@@ -4,7 +4,7 @@
 #define ANNY_WIFI_SSID       "Claro3484898"
 #define ANNY_WIFI_PASSWORD   "D0C65B8B7A91"
 
-#define ANNY_CAMERA_ID       "camera_da5a8401d81c"
-#define ANNY_DEVICE_SECRET   "ne79Ia2EC6topduL8REv6GpUf0_5YGu7Jmv5xillACs"
+#define ANNY_CAMERA_ID       "camera_241a77a609e8"
+#define ANNY_DEVICE_SECRET   "baiB6EMILklg-iTp51Et4fGLVtRc5IgQw2-ggnpWQoo"
 #define ANNY_SERVER_HOST     "3.15.63.191"
 #define ANNY_SERVER_PORT     8554
